da297b3832928060c1091e38cf2f430babc3313b
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-12 15:42:42 +0100

    Add project files.

---
+++ CSSaveToFileSysCLIFW/Program.cs
@@ -0,0 +1,42 @@
+﻿using CSSaveToFileSysFW;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSSaveToFileSysCLIFW
+{
+    class Program
+    {
+        private static int sqlConnectionString = 0;
+        private static int templateName = 1;
+        private static int outPath = 2;
+        private static int outPathProcess = 3;
+        private static int outPathSuccess = 4;
+        private static int outPathError = 5;
+
+        static void Main(string[] args)
+        {
+
+            if (args.Length != 6)
+            {
+                Console.WriteLine(args.Length);
+                Console.WriteLine("<sqlConnectionString> <templateName> <outPath> <outPathProcess> <outPathSuccess> <outPathError>");
+            }
+
+            string[] files =
+                Directory.GetFiles(args[outPath], "*.xml", SearchOption.TopDirectoryOnly);
+
+            new SaveToFileSysFW(
+            args[sqlConnectionString],
+            args[templateName],
+            args[outPath],
+            args[outPathProcess],
+            args[outPathSuccess],
+            args[outPathError]
+            ).Load();
+        }
+    }
+}
