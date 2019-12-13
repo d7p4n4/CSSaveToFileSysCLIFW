44fe51eb1405ecbb50bea2c9078174bb78aa6bf3
d7p4n4 <bercikezs199999999@gmail.com>
2019-12-13 13:17:05 +0100

    1.20191213.1

--- CSSaveToFileSysCLIFW/Program.cs
+++ CSSaveToFileSysCLIFW/Program.cs
@@ -13,9 +13,6 @@ namespace CSSaveToFileSysCLIFW
         private static int sqlConnectionString = 0;
         private static int templateName = 1;
         private static int outPath = 2;
-        private static int outPathProcess = 3;
-        private static int outPathSuccess = 4;
-        private static int outPathError = 5;
 
         static void Main(string[] args)
         {
@@ -23,7 +20,7 @@ namespace CSSaveToFileSysCLIFW
             if (args.Length != 6)
             {
                 Console.WriteLine(args.Length);
-                Console.WriteLine("<sqlConnectionString> <templateName> <outPath> <outPathProcess> <outPathSuccess> <outPathError>");
+                Console.WriteLine("<sqlConnectionString> <templateName> <outPath>");
             }
 
             string[] files =
@@ -32,11 +29,8 @@ namespace CSSaveToFileSysCLIFW
             new SaveToFileSysFW(
             args[sqlConnectionString],
             args[templateName],
-            args[outPath],
-            args[outPathProcess],
-            args[outPathSuccess],
-            args[outPathError]
-            ).Load();
+            args[outPath]
+            ).WriteOutAc4yObject();
         }
     }
 }
